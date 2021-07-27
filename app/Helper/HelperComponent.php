2103891790b649bd14ba7a0a359484baa37d04d4
Wasit Mirani <44178287+wasitmirani@users.noreply.github.com>
2021-07-27 12:04:32 -0700

    fix aligment

--- app/Helper/HelperComponent.php
+++ app/Helper/HelperComponent.php
@@ -8,35 +8,35 @@ class HelperComponent
     public static function SideBar()
     {
         return [
-            [
-                "heading" => "Management",
-                "menu" => [
-                    "title" => 'User Management',
-                    "icon" => "people",
-                    "color_code" => "",
-                    "can" => "",
-                    "v-can" => "",
-                    "sub_menu" => [
-                        setSubMenu(
-                            null,
-                            "Users",
-                            "supervisor_account",
-                            null,
-                            null,
-                            "/users",
-                        ),
-                        setSubMenu(
-                            null,
-                            "Roles",
-                            null,
-                            null,
-                            null,
-                            "/roles",
-                        ),
-                    ]
-                ]
+            // [
+            //     "heading" => "Management",
+            //     "menu" => [
+            //         "title" => 'User Management',
+            //         "icon" => "people",
+            //         "color_code" => "",
+            //         "can" => "",
+            //         "v-can" => "",
+            //         "sub_menu" => [
+            //             setSubMenu(
+            //                 null,
+            //                 "Users",
+            //                 "supervisor_account",
+            //                 null,
+            //                 null,
+            //                 "/users",
+            //             ),
+            //             setSubMenu(
+            //                 null,
+            //                 "Roles",
+            //                 null,
+            //                 null,
+            //                 null,
+            //                 "/roles",
+            //             ),
+            //         ]
+            //     ]
 
-            ],
+            // ],
             [
                 "heading" => "Content",
                 "menu" => [
